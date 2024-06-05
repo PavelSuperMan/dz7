e3088cffc85de6159caac1bb1ad41ab409f40675
Pavel Soroka <sorockapaul@yandex.by>
2024-06-05 23:05:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+// // using System; 
+
+// class Program
+// { 
+//     static void Main() 
+//     { 
+//         Console.Write("Введите значение M: "); 
+//         int M = int.Parse(Console.ReadLine()); 
+
+//         Console.Write("Введите значение N: "); 
+//         int N = int.Parse(Console.ReadLine()); 
+
+//         PrintNaturalNumbers(M, N); 
+//     } 
+
+//     static void PrintNaturalNumbers(int current, int N) 
+//     { 
+//         if (current <= N) 
+//         { 
+//             Console.Write(current + " "); 
+//             PrintNaturalNumbers(current + 1, N); 
+//         } 
+//     } 
+// }
+
+
+
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Введите неотрицательное число m: ");
+//         int m = int.Parse(Console.ReadLine());
+
+//         Console.Write("Введите неотрицательное число n: ");
+//         int n = int.Parse(Console.ReadLine());
+
+//         int result = AckermannFunction(m, n);
+//         Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно: {result}");
+//     }
+
+//     static int AckermannFunction(int m, int n)
+//     {
+//         if (m == 0)
+//             return n + 1;
+//         else if (n == 0)
+//             return AckermannFunction(m - 1, 1);
+//         else
+//             return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+//     }
+// }
+
+
+
+
+// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         int[] myArray = { 15, 10, 36, 11, 50, 47, 90 };
+
+//         Console.WriteLine("Элементы массива, начиная с конца:");
+//         PrintArrayReverse(myArray, myArray.Length - 1);
+//     }
+
+//     static void PrintArrayReverse(int[] arr, int index)
+//     {
+//         if (index >= 0)
+//         {
+//             Console.Write(arr[index] + " ");
+//             PrintArrayReverse(arr, index - 1);
+//         }
+//     }
+// }
